699f5e14d5c4372b36993a6458646c641a9de632
Tshiamo12345 <apollosejake@gmail.com>
2026-09-09 10:37:12 +0200

    added a answer model to track answers made by the attempter

---
+++ src/main/java/com/example/simplequiz/model/Answer.java
@@ -0,0 +1,69 @@
+package com.example.simplequiz.model;
+
+import jakarta.persistence.*;
+
+public class Answer {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "attempt_id",nullable = false)
+    private QuizAttempt quizAttempt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    @Column(nullable = false)
+    private String selectedOption;
+
+    private Boolean isCorrect;
+
+    public Answer() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public QuizAttempt getQuizAttempt() {
+        return quizAttempt;
+    }
+
+    public void setQuizAttempt(QuizAttempt quizAttempt) {
+        this.quizAttempt = quizAttempt;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    public String getSelectedOption() {
+        return selectedOption;
+    }
+
+    public void setSelectedOption(String selectedOption) {
+        this.selectedOption = selectedOption;
+    }
+
+    public Boolean getCorrect() {
+        return isCorrect;
+    }
+
+    public void setCorrect(Boolean correct) {
+        isCorrect = correct;
+    }
+
+
+}
