0b309c63461db6d02d14b3c1f599211d6950e23b
Tshiamo12345 <apollosejake@gmail.com>
2026-09-10 08:21:45 +0200

    add a sql function to delete a pending that is not verified

--- src/main/java/com/example/simplequiz/repository/PendingUserRepository.java
+++ src/main/java/com/example/simplequiz/repository/PendingUserRepository.java
@@ -4,6 +4,7 @@ import com.example.simplequiz.model.PendingUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +12,5 @@ public interface PendingUserRepository extends JpaRepository<PendingUser,String>
 
     Optional<PendingUser> findByUsername(String username);
     Optional<PendingUser> findByEmail(String email);
+    void deleteByExpiryTimestampBefore(LocalDateTime now);
 }
